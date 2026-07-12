aaac8c7b38458f11a22e7741e380299d88a47881
murali110526 <krishna.dba1209@gmail.com>
2026-07-12 11:57:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe,ma
 if ingredients_list:
     ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
+	for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
 
     st.subheader(fruit_chosen + ' Nutrition Information')
