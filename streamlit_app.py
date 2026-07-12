0767f11141429cb36247425d7715611188c260bc
murali110526 <krishna.dba1209@gmail.com>
2026-07-12 11:28:40 +0530

    Update streamlit_app.py
    updated
    st.text(smoothiefroot_response.json())

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+import requests
 
 # Write directly to the app
 st.title("My parents New Healthy Diner")
@@ -17,9 +18,8 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-import requests  
 smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
