4e0cbfd2864846c6c8a25d9ead9751c2567c81a9
murali110526 <krishna.dba1209@gmail.com>
2026-07-12 11:26:11 +0530

    Update streamlit_app.py
     updated below
    import requests  
    smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
    st.text(smoothiefroot_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,11 @@ st.write('The name on your smoothie will be:', name_on_order)
 #session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
+
+import requests  
+smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
